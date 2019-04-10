97d15233cf47ce1df5021763a2e752afd9214252
Ammar Ali <funnyguy118@gmail.com>
2019-04-10 09:12:01 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ function getForm() {
   return form = document.querySelector(`#dog-form`)
 }
 
+
 const getDogs= ()=> {
   fetch(URL)
   .then(res => res.json())
@@ -12,7 +13,7 @@ const getDogs= ()=> {
 
 const makeDogRow= (dog)=> {
   document.querySelector('#table-body').innerHTML += `
-    <tr>
+    <tr id=${dog.id}>
       <td class=padding>${dog.name}</td>
       <td class=padding>${dog.breed}</td>
       <td class=padding>${dog.sex}</td>
@@ -36,24 +37,49 @@ const fillFormData= (row)=> {
   form.elements.name.value = row.children[0].innerText
   form.elements.breed.value = row.children[1].innerText
   form.elements.sex.value = row.children[2].innerText
+  form.id = row.id
 }
 
+const getFormData= ()=> {
+  getForm()
+  return {
+    name: form.elements.name.value,
+    breed:form.elements.breed.value,
+    sex: form.elements.sex.value,
+    id: form.id
+  }
+}
 
+const addSubmitListener= ()=> {
+  getForm().addEventListener('submit', (event)=> {
+    let formName = event.target.children.name.value
+    let formBreed = event.target.children.breed.value
+    let formSex = event.target.children.sex.value
 
-const updateDogs= (dog)=> {
+    let objectRow = {name:formName, breed:formBreed, sex:formSex}
+
+    let num = event.target.id
+    updateDogs(objectRow, num)
+  })
+}
+
+const updateDogs= (row, id)=> {
   const patch = {
     method: `PATCH`,
     headers: {"Content-Type": `application/json`, Accept: `application/json`},
-    body: JSON.stringify(dog)
+    body: JSON.stringify(row)
   }
-  fetch(URL+dog.id, patch)
+  fetch(URL+id, patch)
   .then(res => res.json())
   .then(res => {
     console.log("did something", res)
   })
 }
 
-document.addEventListener('DOMContentLoaded', () => {
+const init= ()=> {
   getDogs()
   addEditListener()
-})
+  addSubmitListener()
+}
+
+document.addEventListener('DOMContentLoaded', init)
